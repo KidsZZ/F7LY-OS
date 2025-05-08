c5eda4f8753efb4218ba111bdf23a2a6ac89c6d7
gkq <guankaiqi@whu.edu.cn>
2025-05-08 18:29:03 +0800

    procmm upd

--- kernel/proc/proc_manager.cc
+++ kernel/proc/proc_manager.cc
@@ -1,5 +1,18 @@
 #include"proc_manager.hh"
 #include "hal/cpu.hh"
+#include "physical_memory_manager.hh"
+#include "klib.hh"
+
+extern "C"
+{
+
+    extern int init_main(void);
+
+    void _wrp_fork_ret(void)
+    {
+        proc::k_pm.fork_ret();
+    }
+}
 
 namespace proc
 {
@@ -26,4 +39,69 @@ namespace proc
         Cpu::pop_intr_off();
         return pcb;
     }
+
+    void ProcessManager::alloc_pid(Pcb *p)
+    {
+        _pid_lock.acquire();
+        p->_pid = _cur_pid;
+        _cur_pid++;
+        _pid_lock.release();
+    }
+
+    Pcb *ProcessManager::alloc_proc()
+    {
+        Pcb *p;
+        for (uint i = 0; i < num_process; i++)
+        {
+            p = &k_proc_pool[(_last_alloc_proc_gid + i) % num_process];
+            p->_lock.acquire();
+            if (p->_state == ProcState::UNUSED)
+            {
+                k_pm.alloc_pid(p);
+                p->_state = ProcState::USED;
+                p->_slot = default_proc_slot;
+                p->_priority = default_proc_prio;
+
+                // p->_shm = mem::vml::vm_trap_frame - 64 * 2 * mem::PageEnum::pg_size;
+                // p->_shmkeymask = 0;
+                // k_pm.set_vma( p );
+
+                if ((p->_trapframe = (TrapFrame *)mem::k_pmm.alloc_page()) == nullptr)
+                {
+                    freeproc(p);
+                    p->_lock.release();
+                    return nullptr;
+                }
+
+                _proc_create_vm(p);
+                if (p->_pt.get_base() == 0)
+                {
+                    freeproc(p);
+                    p->_lock.release();
+                    return nullptr;
+                }
+
+                p->_mqmask = 0;
+
+                memset(&p->_context, 0, sizeof(p->_context));
+
+                p->_context.ra = (uint64)_wrp_fork_ret;
+
+                p->_context.sp = p->_kstack + PGSIZE;
+
+                p->_lock.release();
+
+                _last_alloc_proc_gid = p->_gid;
+
+                return p;
+            }
+            else
+            {
+                p->_lock.release();
+            }
+        }
+        return nullptr;
+    }
+
+    
 }
