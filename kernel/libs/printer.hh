affbf9779bacf770dfd17ff338cdbaae0e36c692
Dandelion <1169809321@qq.com>
2025-05-22 22:06:44 +0800

    noting

--- kernel/libs/printer.hh
+++ kernel/libs/printer.hh
@@ -9,10 +9,9 @@
 // 		out_error,
 // 		out_panic,
 // 	};
-#define panic(info,args...) k_printer.k_panic( __FILE__, __LINE__, info,##args )
-#define printf(info,args...) k_printer.print(info,##args)
+#define panic(info, args...) k_printer.k_panic(__FILE__, __LINE__, info, ##args)
+#define printf(info, args...) k_printer.print(info, ##args)
 #define assert(expr, detail, args...) ((expr) ? (void)0 : k_printer.assrt(__FILE__, __LINE__, #expr, detail, ##args))
-
 
 #ifndef COLOR_PRINT
 #define COLOR_PRINT
@@ -29,87 +28,94 @@
 #define Info_R(fmt, ...) printfRed("[INFO] => " fmt "", ##__VA_ARGS__)
 
 // TODO macro
-#define TODO(x) 
+#define TODO(x)
 #endif
 
 class Printer
 {
-    private:
-		enum out_type
-		{
-			console,
-			file,
-			device,
-		};
-        out_type _type;
-		Console *	_console;
-        SpinLock		_lock;
-		int					_locking  = 1;
-		int					_panicked = 0;
-		static int	_trace_flag;
-		static char _lower_digits[];
-		static char _upper_digits[];
-    public:
-        Printer() {}
-    
-		void init();
-        inline int is_panic() { return _panicked; }
-    
-		void print( const char *fmt, ... );
-        void printint( int xx, int base, int sign );
-		void printbyte( uint8 x );
-		void printptr( uint64 x );
+private:
+	enum out_type
+	{
+		console,
+		file,
+		device,
+	};
+	out_type _type;
+	Console *_console;
+	SpinLock _lock;
+	int _locking = 1;
+	int _panicked = 0;
+	static int _trace_flag;
+	static char _lower_digits[];
+	static char _upper_digits[];
 
-		static void k_panic( const char *f, uint l, const char *info, ... );
-		static void panic_va( const char *f, uint l, const char *info, va_list ap );
-		static void error( const char *f, uint l, const char *info, ... );
-		static void error_va( const char *f, uint l, const char *info, va_list ap );
-		static void warn( const char *f, uint l, const char *info, ... );
-		static void warn_va( const char *f, uint l, const char *info, va_list ap );
-		static void info( const char *f, uint l, const char *info, ... );
-		static void info_va( const char *f, uint l, const char *info, va_list ap );
-		static void trace( const char *f, uint l, const char *info, ... );
-		static void trace_va( const char *f, uint l, const char *info, va_list ap );
-		static void assrt( const char *f, uint l, const char *expr, const char *detail, ... );
-		static void assrt_va( const char *f, uint l, const char *expr, const char *detail,
-							  va_list ap );
-		private:
-		int _divide( ulong &n, int base )
+public:
+	Printer() {}
+
+	void init();
+	inline int is_panic() { return _panicked; }
+
+	void print(const char *fmt, ...);
+	void printint(int xx, int base, int sign);
+	void printbyte(uint8 x);
+	void printptr(uint64 x);
+
+	static void k_panic(const char *f, uint l, const char *info, ...);
+	static void panic_va(const char *f, uint l, const char *info, va_list ap);
+	static void error(const char *f, uint l, const char *info, ...);
+	static void error_va(const char *f, uint l, const char *info, va_list ap);
+	static void warn(const char *f, uint l, const char *info, ...);
+	static void warn_va(const char *f, uint l, const char *info, va_list ap);
+	static void info(const char *f, uint l, const char *info, ...);
+	static void info_va(const char *f, uint l, const char *info, va_list ap);
+	static void trace(const char *f, uint l, const char *info, ...);
+	static void trace_va(const char *f, uint l, const char *info, va_list ap);
+	static void assrt(const char *f, uint l, const char *expr, const char *detail, ...);
+	static void assrt_va(const char *f, uint l, const char *expr, const char *detail,
+						 va_list ap);
+
+private:
+	int _divide(ulong &n, int base)
+	{
+		int res = (int)(n % base);
+		n = n / base;
+		return res;
+	}
+
+	bool _is_number(char c) { return (unsigned)(c - '0') < 10; }
+
+	int _to_number(char c) { return c - '0'; }
+
+public:
+	void print(const string &str)
+	{
+		print("%s", str.c_str());
+	}
+		static Printer &endl(Printer & p)
 		{
-			int res = (int) ( n % base );
-			n		= n / base;
-			return res;
+			p.print("\n");
+			return p;
 		}
-    
-		bool _is_number( char c ) { return (unsigned) ( c - '0' ) < 10; }
-    
-		int	 _to_number( char c ) { return c - '0'; }
-    public:
-		void print(const string& str) {
-		print("%s", str.c_str());
-		static Printer& endl(Printer& p) {
-		p.print("\n");
-		return p;
-	}
 
-	// 接收 endl 的 << 运算符
-	Printer& operator<<(Printer& (*func)(Printer&)) {
-		return func(*this);
-	}
+		// 接收 endl 的 << 运算符
+		Printer &operator<<(Printer &(*func)(Printer &))
+		{
+			return func(*this);
+		}
 
-	// string 重载（你已有）
-	Printer& operator<<(const string& str) {
-		print("%s", str.c_str());
-		return *this;
-	}
+		// string 重载（你已有）
+		Printer &operator<<(const string &str)
+		{
+			print("%s", str.c_str());
+			return *this;
+		}
 
-	// const char* 也顺带支持一下
-	Printer& operator<<(const char* str) {
-		print("%s", str);
-		return *this;
-	}
-	}
+		// const char* 也顺带支持一下
+		Printer &operator<<(const char *str)
+		{
+			print("%s", str);
+			return *this;
+		}
+	
 };
 extern Printer k_printer;
-
-
